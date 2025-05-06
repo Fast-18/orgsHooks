92191b33d9996b64670d7da727f5abfd2d4ea9ea
MATHEUS HENRIQUE CARDOSO KRUTLI <krutli.4961@aluno.pr.senac.br>
2025-05-06 07:28:27 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import { SafeAreaView } from 'react-native';
+
+
+function App() {
+  return <SafeAreaView>
+    
+  </SafeAreaView>
+}
+
+export default App;
